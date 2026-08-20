76bb004272d7da4bca4d3c7f55c5ba613c51a2bd
Crombex <pixel-42@yandex.ru>
2026-08-20 16:19:30 +0300

    feat: добавлена сущность пользователя

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,49 @@
-export class User {}
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from 'typeorm'
+// import {Roles} from '........'
+
+
+@Entity('users')
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @Column({type: 'varchar', length: 32})
+  name!: string
+
+  @Column({type: 'varchar', length: 128, unique: true})
+  email!: string
+
+  @Column({type: 'varchar', length: 255})
+  password!: string
+
+  @Column({type: 'text', nullable: true})
+  about!: string
+
+  @Column({type: 'date'})
+  birthdate!: string
+
+  @Column({type: 'varchar', length: 64})
+  city!: string
+
+  @Column({type: 'varchar', length: 16})
+  gender!: string
+
+  @Column({type: 'varchar', length: 255})
+  avatar!: string
+
+  @Column({type: 'enum', enum: [/* Roles.USER, Roles.ADMIN */], default: 'user'})
+  role!: string
+
+  // @OneToMany(() => Skill, skill => skill.user) - Skill пока не создан, поэтому закомментировал
+  skills!: string[]
+
+  @Column({type: 'varchar', length: 255})
+  refreshToken!: string
+
+  // @OneToMany(() => Category, category => category.wantToLearn) - Category пока не создан, поэтому закомментировал
+  wantToLearn!: string[]
+
+  @Column({type: 'simple-array', nullable: true})
+  favoriteSkills!: string[]
+}
