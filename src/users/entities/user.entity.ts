2f52b886b8d0ea620d90d4c16740470069d456d3
Aleksandr Rechkalov <rechkalov.a.a@ya.ru>
2026-08-20 23:42:02 +0500

    feat: add app.config with port and hashSalt

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,49 +1,53 @@
-import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 // import {Roles} from '........'
-
 
 @Entity('users')
 export class User {
-
   @PrimaryGeneratedColumn('uuid')
-  id!: string
+  id!: string;
 
-  @Column({type: 'varchar', length: 32})
-  name!: string
+  @Column({ type: 'varchar', length: 32 })
+  name!: string;
 
-  @Column({type: 'varchar', length: 128, unique: true})
-  email!: string
+  @Column({ type: 'varchar', length: 128, unique: true })
+  email!: string;
 
-  @Column({type: 'varchar', length: 255})
-  password!: string
+  @Column({ type: 'varchar', length: 255 })
+  password!: string;
 
-  @Column({type: 'text', nullable: true})
-  about!: string
+  @Column({ type: 'text', nullable: true })
+  about!: string;
 
-  @Column({type: 'date'})
-  birthdate!: string
+  @Column({ type: 'date' })
+  birthdate!: string;
 
-  @Column({type: 'varchar', length: 64})
-  city!: string
+  @Column({ type: 'varchar', length: 64 })
+  city!: string;
 
-  @Column({type: 'varchar', length: 16})
-  gender!: string
+  @Column({ type: 'varchar', length: 16 })
+  gender!: string;
 
-  @Column({type: 'varchar', length: 255})
-  avatar!: string
+  @Column({ type: 'varchar', length: 255 })
+  avatar!: string;
 
-  @Column({type: 'enum', enum: [/* Roles.USER, Roles.ADMIN */], default: 'user'})
-  role!: string
+  @Column({
+    type: 'enum',
+    enum: [
+      /* Roles.USER, Roles.ADMIN */
+    ],
+    default: 'user',
+  })
+  role!: string;
 
   // @OneToMany(() => Skill, skill => skill.user) - Skill пока не создан, поэтому закомментировал
-  skills!: string[]
+  skills!: string[];
 
-  @Column({type: 'varchar', length: 255})
-  refreshToken!: string
+  @Column({ type: 'varchar', length: 255 })
+  refreshToken!: string;
 
   // @OneToMany(() => Category, category => category.wantToLearn) - Category пока не создан, поэтому закомментировал
-  wantToLearn!: string[]
+  wantToLearn!: string[];
 
-  @Column({type: 'simple-array', nullable: true})
-  favoriteSkills!: string[]
+  @Column({ type: 'simple-array', nullable: true })
+  favoriteSkills!: string[];
 }
