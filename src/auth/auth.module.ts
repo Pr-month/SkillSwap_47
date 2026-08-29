42b98ed71cdd32039fb97369df75ee3705632808
Aleksandr Rechkalov <rechkalov.a.a@ya.ru>
2026-08-29 10:54:28 +0500

    feat(auth): add RolesGuard and Roles decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,6 +12,7 @@ import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
@@ -33,7 +34,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     CategoriesModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, JwtAuthGuard],
-  exports: [JwtModule, JwtAuthGuard],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard, RolesGuard],
+  exports: [JwtModule, JwtAuthGuard, RolesGuard],
 })
 export class AuthModule {}
