717fdcb6076da1514838ea999fd802bc2c624ce4
Aleksandr Rechkalov <rechkalov.a.a@ya.ru>
2026-08-29 12:06:21 +0500

    feat(common): add global all-exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig, IConfig } from './app.config';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new AllExceptionsFilter());
   const config = app.get<IConfig>(appConfig.KEY);
   await app.listen(config.port);
 }
