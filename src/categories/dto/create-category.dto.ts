117b225a962c524c0894ae167dcda43774b16b7a
Aleksandr Rechkalov <rechkalov.a.a@ya.ru>
2026-09-12 10:23:37 +0500

    docs(categories): заполнил swagger для эндпоинтов categories

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUUID, Length } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    description: 'Название категории',
+    example: 'Музыка',
+    minLength: 1,
+    maxLength: 128,
+  })
   @IsString()
   @Length(1, 128)
   name!: string;
 
+  @ApiPropertyOptional({
+    description: 'ID корневой категории, если создаём подкатегорию',
+    format: 'uuid',
+  })
   @IsOptional()
   @IsUUID()
   parentId?: string;
